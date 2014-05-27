79b3ba67f4d93ed17fa0c70b7a02c259018f6609
rkusa <m@rkusa.st>
2014-05-27 17:37:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# riakpb-stream
+
+[![NPM][npm]](https://npmjs.org/package/riakpb-stream)
+[![Build Status](travis)](http://travis-ci.org/rkusa/riakpb-stream)
+[![Dependency Status][deps]](https://david-dm.org/rkusa/riakpb-stream)
+
+[riak_pb](https://github.com/basho/riak_pb) version [`2.0.0.15`](https://github.com/basho/riak_pb/tree/2.0.0.15)
+
+## MIT License
+
+Copyright (c) 2014 Markus Ast
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[npm]: https://badge.fury.io/js/riakpb-stream.svg
+[travis]: https://secure.travis-ci.org/rkusa/riakpb-stream.svg
+[deps]: https://david-dm.org/rkusa/riakpb-stream.png?theme=shields.io
---
+++ test/test.js
@@ -0,0 +1,107 @@
+/*eslint-env node, mocha */
+
+'use strict'
+
+var stream = require('stream')
+var net = require('net')
+var assert = require('assert').ok
+var frame = require('frame-stream')
+var riak = require('../lib')
+var Messages = require('../lib/messages')
+var messages = new Messages
+var server
+
+setup(function(done) {
+  // mock riak
+  server = net.createServer(function(socket) {
+    var framer = new frame.FramerStream()
+
+    framer
+    .pipe(socket)
+    .pipe(frame())
+    .pipe(through({}, function(chunk, enc, cont) {
+      var type = chunk.readInt8(0), message
+      switch (type) {
+        case 1: // RpbPingReq
+          message = new Buffer(1)
+          message.writeInt8(2, 0)
+          framer.write(message)
+          break
+        case 17: // RpbListKeysReq
+          var body = messages.serialize({ keys: ['a', 'b'], done: true }, 'RpbListKeysResp')
+          message = new Buffer(body.length + 1)
+          message.writeInt8(18, 0)
+          body.copy(message, 1)
+          framer.write(message)
+          break
+      }
+      cont()
+    }))
+  })
+  server.listen(4000, done)
+})
+
+setup(function(done) {
+  if (messages.isReady) done()
+  else messages.once('ready', done)
+})
+
+teardown(function(done) {
+  server.close(done)
+})
+
+test('message without content', function(done) {
+  var socket = net.connect(4000, function() {
+    var serializer = new riak.Serializer
+    var parser = new riak.Parser
+
+    serializer
+    .pipe(new frame.FramerStream())
+    .pipe(socket)
+    .pipe(frame())
+    .pipe(parser)
+    .pipe(through({ objectMode: true }, function(chunk, enc, cont) {
+      assert(chunk === true)
+      cont()
+      socket.end()
+      done()
+    }))
+
+    serializer.write('RpbPingReq')
+  })
+})
+
+test('message with content', function(done) {
+  var socket = net.connect(4000, function() {
+    var serializer = new riak.Serializer
+    var parser = new riak.Parser
+
+    serializer
+    .pipe(new frame.FramerStream())
+    .pipe(socket)
+    .pipe(frame())
+    .pipe(parser)
+    .pipe(through({ objectMode: true }, function(chunk, enc, cont) {
+      assert(chunk.keys[0].toString() === 'a')
+      assert(chunk.keys[1].toString() === 'b')
+      assert(chunk.done === true)
+      cont()
+      socket.end()
+      done()
+    }))
+
+    serializer.write({ bucket: 'test' }, 'RpbListKeysReq')
+  })
+})
+
+function through(opts, transform, flush) {
+  var t = new stream.Transform(opts)
+
+  t._transform = transform
+
+  if (flush) {
+    t._flush = flush
+  }
+
+  return t
+}
